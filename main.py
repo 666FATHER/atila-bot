59959441a84eceeec45b58c61e0a1f06225a00c9
666FATHER <133801145+666FATHER@users.noreply.github.com>
2026-09-11 19:04:30 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
 import os, requests, telebot
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-bot = telebot.TeleBot(BOT_TOKEN)
+TOKEN = os.getenv("BOT_TOKEN")
+bot = telebot.TeleBot(TOKEN)
 bot.delete_webhook(drop_pending_updates=True)
-
 @bot.message_handler(commands=['marea','start'])
-def marea(m):
+def h(m):
     try:
         p = float(requests.get("https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT", timeout=10).json()['price'])
     except:
-        p = 77271.0
-    bot.reply_to(m, f"🌊 ATILA MAREA\nBTC: {p}\n77108 SOPORTE\n77441 TECHO")
+        p = 77271
+    bot.reply_to(m, f"🌊 ATILA MAREA\nBTC: {p}\n77108 SOPORTE\n77441 LIQ TECHO")
 
-print("BOT PRENDIDO")
 bot.infinity_polling()
